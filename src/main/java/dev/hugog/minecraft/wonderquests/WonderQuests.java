a271d52cb1f55981b9aae34d048fbc7f81d78200
Hugo Gonçalves <hugomiguel2001.13@gmail.com>
2024-01-06 23:22:38 +0000

    Added database checks on plugin initialization.

--- src/main/java/dev/hugog/minecraft/wonderquests/WonderQuests.java
+++ src/main/java/dev/hugog/minecraft/wonderquests/WonderQuests.java
@@ -1,28 +1,53 @@
 package dev.hugog.minecraft.wonderquests;
 
+import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
+import dev.hugog.minecraft.wonderquests.data.connectivity.DataSource;
+import dev.hugog.minecraft.wonderquests.data.connectivity.DbInitializer;
 import dev.hugog.minecraft.wonderquests.injection.BasicBinderModule;
+import java.util.concurrent.CompletableFuture;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WonderQuests extends JavaPlugin {
+
+  @Inject
+  private DataSource dataSource;
+
+  @Inject
+  private DbInitializer dbInitializer;
 
   @Override
   public void onEnable() {
 
     initDependencyInjectionModule();
 
-    getLogger().info("Plugin successfully enabled!");
+    dataSource.initDataSource("localhost", "5432", "wonder_quests",
+        "postgres", "admin");
+
+    // Check the state of the database. Correct the state if necessary.
+    CompletableFuture<Void> databaseCheckFuture = dbInitializer.checkDatabase();
+
+    CompletableFuture.allOf(databaseCheckFuture).whenComplete((value, throwable) -> {
+      if (throwable == null) {
+        getLogger().info("Plugin successfully enabled!");
+      }
+    });
 
   }
 
   @Override
   public void onDisable() {
+
+    dataSource.closeDataSource();
+
     getLogger().info("Plugin successfully disabled!");
+
   }
 
   private void initDependencyInjectionModule() {
     BasicBinderModule guiceBinderModule = new BasicBinderModule(this);
-    Injector injector = guiceBinderModule.createInjector();
+    Injector injector = Guice.createInjector(guiceBinderModule);
     injector.injectMembers(this);
   }
 
