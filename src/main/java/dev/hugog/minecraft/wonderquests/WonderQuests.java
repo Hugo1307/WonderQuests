179025c3965d4bcd851c5e0e0941a0edda3b96e0
Hugo Gonçalves <hugomiguel2001.13@gmail.com>
2024-01-06 16:12:03 +0000

    Initialized Repository

---
+++ src/main/java/dev/hugog/minecraft/wonderquests/WonderQuests.java
@@ -0,0 +1,17 @@
+package dev.hugog.minecraft.wonderquests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WonderQuests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
