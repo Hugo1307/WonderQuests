38d490504ddf1a4809d7b91744e1bdc23eeb0967
Hugo Gonçalves <hugomiguel2001.13@gmail.com>
2024-01-07 21:51:18 +0000

    Refactored wait for multiple futures to use ConcurrencyHandler.

--- src/main/java/dev/hugog/minecraft/wonderquests/WonderQuests.java
+++ src/main/java/dev/hugog/minecraft/wonderquests/WonderQuests.java
@@ -3,8 +3,10 @@ package dev.hugog.minecraft.wonderquests;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import dev.hugog.minecraft.wonderquests.concurrency.ConcurrencyHandler;
 import dev.hugog.minecraft.wonderquests.data.connectivity.DataSource;
 import dev.hugog.minecraft.wonderquests.data.connectivity.DbInitializer;
+import dev.hugog.minecraft.wonderquests.data.services.QuestsService;
 import dev.hugog.minecraft.wonderquests.injection.BasicBinderModule;
 import java.util.concurrent.CompletableFuture;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,22 +19,33 @@ public final class WonderQuests extends JavaPlugin {
   @Inject
   private DbInitializer dbInitializer;
 
+  @Inject
+  private QuestsService questsService;
+
+  @Inject
+  private ConcurrencyHandler concurrencyHandler;
+
   @Override
   public void onEnable() {
 
     initDependencyInjectionModule();
 
-    dataSource.initDataSource("localhost", "5432", "wonder_quests",
-        "postgres", "admin");
+    dataSource.initDataSource("localhost", "5432", "wonder_quests", "postgres", "admin");
 
     // Check the state of the database. Correct the state if necessary.
     CompletableFuture<Void> databaseCheckFuture = dbInitializer.checkDatabase();
+    CompletableFuture<?>[] futuresToWaitFor = concurrencyHandler.getListOfFutures(databaseCheckFuture);
 
-    CompletableFuture.allOf(databaseCheckFuture).whenComplete((value, throwable) -> {
-      if (throwable == null) {
-        getLogger().info("Plugin successfully enabled!");
-      }
-    });
+    // Sync Logic
+
+    concurrencyHandler.runAfterMultiple(futuresToWaitFor, () -> {
+
+      // Optional<QuestDto> questDto = questsService.getQuestById(22312).join();
+      // getLogger().info("Quest: " + questDto);
+
+      getLogger().info("Plugin successfully enabled!");
+
+    }, true);
 
   }
 
