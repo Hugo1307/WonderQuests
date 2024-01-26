4c0811c4e96207003e08fd3ff499d01502c5bd3a
Hugo Gonçalves <hugomiguel2001.13@gmail.com>
2024-01-26 14:15:57 +0000

    Improved quest item lore on Available Quests GUI.

--- src/main/java/dev/hugog/minecraft/wonderquests/guis/AvailableQuestsGui.java
+++ src/main/java/dev/hugog/minecraft/wonderquests/guis/AvailableQuestsGui.java
@@ -5,6 +5,8 @@ import com.google.inject.assistedinject.Assisted;
 import dev.hugog.minecraft.wonderquests.WonderQuests;
 import dev.hugog.minecraft.wonderquests.concurrency.ConcurrencyHandler;
 import dev.hugog.minecraft.wonderquests.data.dtos.QuestDto;
+import dev.hugog.minecraft.wonderquests.data.dtos.QuestRewardDto;
+import dev.hugog.minecraft.wonderquests.data.dtos.requirements.QuestRequirementDto;
 import dev.hugog.minecraft.wonderquests.injection.factories.ActionsFactory;
 import dev.hugog.minecraft.wonderquests.mediators.QuestsMediator;
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import java.util.concurrent.CompletableFuture;
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
@@ -96,28 +99,68 @@ public class AvailableQuestsGui implements Gui {
     List<Component> itemLore = new ArrayList<>();
 
     ItemMeta questItemMeta = questItem.getItemMeta();
-    questItemMeta.displayName(Component.text(quest.getName()));
+    questItemMeta.displayName(Component.text(quest.getName())
+        .color(NamedTextColor.GREEN)
+        .decorate(TextDecoration.BOLD)
+    );
 
-    itemLore.add(Component.text(quest.getDescription()));
+    Component loreHeader = Component.empty()
+        .append(Component.text(quest.getDescription(), NamedTextColor.GRAY));
 
-    itemLore.add(Component.text(""));
-    itemLore.add(Component.text("Requirements:"));
-    itemLore.add(Component.text(""));
-    quest.getRequirements().forEach((requirement) -> {
-      itemLore.add(Component.text(requirement.obtainRepresentation()));
-    });
-    itemLore.add(Component.text(""));
-    itemLore.add(Component.text("Rewards:"));
-    itemLore.add(Component.text(""));
-    quest.getRewards().forEach((reward) -> {
-      itemLore.add(Component.text(reward.obtainRepresentation()));
-    });
-    itemLore.add(Component.text(""));
-    itemLore.add(Component.text("Objective:"));
-    itemLore.add(Component.text(""));
-    itemLore.add(Component.text(quest.getObjective().obtainRepresentation()));
+    itemLore.add(loreHeader);
+    itemLore.add(Component.empty());
 
-    itemLore.add(Component.text(""));
+    Component objectiveComponent = Component.empty()
+        .append(Component.text("Objective:", NamedTextColor.GREEN))
+        .appendSpace()
+        .append(Component.text(quest.getObjective().obtainRepresentation(), NamedTextColor.GRAY));
+
+    itemLore.add(objectiveComponent);
+    itemLore.add(Component.empty());
+
+    Component requirementsComponent = Component.text("Requirements", NamedTextColor.GREEN);
+
+    itemLore.add(requirementsComponent);
+
+    if (quest.getRequirements().isEmpty()) {
+      itemLore.add(Component.text("None", NamedTextColor.GRAY));
+    }
+
+    for (QuestRequirementDto requirement : quest.getRequirements()) {
+
+      Component individualRequirementComponent = Component.empty()
+          .appendSpace()
+          .appendSpace()
+          .append(Component.text("• ", NamedTextColor.GREEN))
+          .append(Component.text(requirement.obtainRepresentation(), NamedTextColor.GRAY));
+
+      itemLore.add(individualRequirementComponent);
+
+    }
+
+    itemLore.add(Component.empty());
+
+    Component rewardsComponent = Component.text("Rewards", NamedTextColor.GREEN);
+
+    itemLore.add(rewardsComponent);
+
+    if (quest.getRewards().isEmpty()) {
+      itemLore.add(Component.text("None", NamedTextColor.GRAY));
+    }
+
+    for (QuestRewardDto reward : quest.getRewards()) {
+
+      Component individualRewardComponent = Component.empty()
+          .appendSpace()
+          .appendSpace()
+          .append(Component.text("• ", NamedTextColor.GREEN))
+          .append(Component.text(reward.obtainRepresentation(), NamedTextColor.GRAY));
+
+      itemLore.add(individualRewardComponent);
+
+    }
+
+    itemLore.add(Component.empty());
 
     questItemMeta.lore(itemLore);
 
