9e57f7b26894216c1e8e1833ecc94e558c9bd3bb
Hugo Gonçalves <hugomiguel2001.13@gmail.com>
2024-01-26 21:27:51 +0000

    Added documentation for ConcurrencyHandler.

--- src/main/java/dev/hugog/minecraft/wonderquests/concurrency/ConcurrencyHandler.java
+++ src/main/java/dev/hugog/minecraft/wonderquests/concurrency/ConcurrencyHandler.java
@@ -13,6 +13,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+/**
+ * This class handles the concurrency in the application.
+ */
 @Singleton
 public class ConcurrencyHandler {
 
@@ -20,6 +23,11 @@ public class ConcurrencyHandler {
 
   private final WonderQuests plugin;
 
+  /**
+   * Constructor for the ConcurrencyHandler class.
+   *
+   * @param plugin The plugin instance used for scheduling tasks.
+   */
   @Inject
   public ConcurrencyHandler(WonderQuests plugin) {
 
@@ -32,10 +40,21 @@ public class ConcurrencyHandler {
 
   }
 
+  /**
+   * Constructor for the ConcurrencyHandler class.
+   * This constructor is used for testing purposes.
+   */
   public ConcurrencyHandler() {
     this(null);
   }
 
+  /**
+   * This method runs a task.
+   *
+   * @param runnable The task to be run.
+   * @param async A boolean indicating if the task should be run asynchronously.
+   * @return a CompletableFuture that will be completed when the task is run.
+   */
   public CompletableFuture<Void> run(Runnable runnable, boolean async) {
 
     if (async) {
@@ -47,6 +66,14 @@ public class ConcurrencyHandler {
 
   }
 
+  /**
+   * This method runs a task after multiple other tasks are completed.
+   *
+   * @param completableFutures The tasks to be completed before running the task.
+   * @param runnable The task to be run.
+   * @param async A boolean indicating if the task should be run asynchronously.
+   * @return a CompletableFuture that will be completed when the task is run.
+   */
   public CompletableFuture<Void> runAfterMultiple(CompletableFuture<?>[] completableFutures,
       Runnable runnable, boolean async) {
 
@@ -60,6 +87,15 @@ public class ConcurrencyHandler {
 
   }
 
+  /**
+   * This method runs a task after a delay.
+   *
+   * @param runnable The task to be run.
+   * @param delay The delay before running the task.
+   * @param timeUnit The unit of the delay.
+   * @param async A boolean indicating if the task should be run asynchronously.
+   * @return a CompletableFuture that will be completed when the task is run.
+   */
   public CompletableFuture<Void> runDelayed(Runnable runnable, long delay, TimeUnit timeUnit,
       boolean async) {
 
@@ -74,6 +110,13 @@ public class ConcurrencyHandler {
 
   }
 
+  /**
+   * This method supplies a result.
+   *
+   * @param supplier The supplier of the result.
+   * @param async A boolean indicating if the result should be supplied asynchronously.
+   * @return a CompletableFuture that will be completed with the result.
+   */
   public <T> CompletableFuture<T> supply(Supplier<T> supplier, boolean async) {
 
     if (async) {
@@ -84,6 +127,14 @@ public class ConcurrencyHandler {
 
   }
 
+  /**
+   * This method accepts a result after a task is completed.
+   *
+   * @param completableFuture The task to be completed before accepting the result.
+   * @param consumer The consumer of the result.
+   * @param async A boolean indicating if the result should be accepted asynchronously.
+   * @return a CompletableFuture that will be completed when the result is accepted.
+   */
   public <T> CompletableFuture<Void> thenAccept(CompletableFuture<T> completableFuture,
       Consumer<? super T> consumer, boolean async) {
 
@@ -96,10 +147,21 @@ public class ConcurrencyHandler {
 
   }
 
+  /**
+   * This method gets a list of futures.
+   *
+   * @param futures The futures to be included in the list.
+   * @return an array of CompletableFutures.
+   */
   public CompletableFuture<?>[] getListOfFutures(CompletableFuture<?>... futures) {
     return futures;
   }
 
+  /**
+   * This method runs a task on the main thread.
+   *
+   * @param runnable The task to be run.
+   */
   public void runOnMainThread(Runnable runnable) {
     plugin.getServer().getScheduler().runTask(plugin, runnable);
   }
