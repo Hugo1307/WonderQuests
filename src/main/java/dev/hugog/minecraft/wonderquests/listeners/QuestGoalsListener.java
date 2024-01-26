fdf0031725fbdb608c2e55847eca9de6e99b46a2
Hugo Gonçalves <hugomiguel2001.13@gmail.com>
2024-01-26 14:41:23 +0000

    Added Kill Mobs goal type for quests.

--- src/main/java/dev/hugog/minecraft/wonderquests/listeners/QuestGoalsListener.java
+++ src/main/java/dev/hugog/minecraft/wonderquests/listeners/QuestGoalsListener.java
@@ -11,11 +11,13 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class QuestGoalsListener implements Listener {
 
@@ -151,118 +153,67 @@ public class QuestGoalsListener implements Listener {
 
   }
 
-//  @EventHandler
-//  public void onItemCraft(CraftItemEvent event) {
-//
-//    Player player = (Player) event.getWhoClicked();
-//
-//    playerService.getCurrentActiveQuests(player.getUniqueId())
-//        .thenAccept((activeQuests) -> {
-//
-//          // The player doesn't have any active quests - no need to continue
-//          if (activeQuests.isEmpty()) {
-//            return;
-//          }
-//
-//          activeQuests.forEach((activeQuest) -> {
-//            questsService.getQuestById(activeQuest.getQuestId()).thenAccept((quest) -> {
-//
-//              // The quest doesn't exist
-//              if (quest.isEmpty()) {
-//                return;
-//              }
-//
-//              QuestObjectiveDto objective = quest.get().getObjective();
-//              if (objective.getType() == ObjectiveType.CRAFT_ITEM) {
-//                activeQuest.setProgress(activeQuest.getProgress() + 1);
-//              }
-//
-//            });
-//          });
-//        });
-//
-//  }
-//
-//  @EventHandler
-//  public void onMobKill(EntityDeathEvent event) {
-//
-//    Player player = event.getEntity().getKiller();
-//
-//    // If the player didn't kill the mob, we don't need to handle this event
-//    if (player == null) {
-//      return;
-//    }
-//
-//    playerService.getCurrentActiveQuests(player.getUniqueId())
-//        .thenAccept((activeQuests) -> {
-//
-//          // The player doesn't have any active quests - no need to continue
-//          if (activeQuests.isEmpty()) {
-//            return;
-//          }
-//
-//          activeQuests.forEach((activeQuest) -> {
-//            questsService.getQuestById(activeQuest.getQuestId()).thenAccept((quest) -> {
-//
-//              // The quest doesn't exist
-//              if (quest.isEmpty()) {
-//                return;
-//              }
-//
-//              QuestObjectiveDto objective = quest.get().getObjective();
-//              if (objective.getType() == ObjectiveType.KILL_MOBS) {
-//                activeQuest.setProgress(activeQuest.getProgress() + 1);
-//              }
-//
-//            });
-//          });
-//        });
-//
-//  }
-//
-//  @EventHandler
-//  public void onPlayerKill(EntityDeathEvent event) {
-//
-//    Player player = event.getEntity().getKiller();
-//
-//    // If the player didn't kill the mob, we don't need to handle this event
-//    if (player == null) {
-//      return;
-//    }
-//
-//    // If the player killed another entity that was not a player, we don't need to handle this event
-//    if (!(event.getEntity() instanceof Player)) {
-//      return;
-//    }
-//
-//    playerService.getCurrentActiveQuests(player.getUniqueId())
-//        .thenAccept((activeQuests) -> {
-//
-//          // The player doesn't have any active quests - no need to continue
-//          if (activeQuests.isEmpty()) {
-//            return;
-//          }
-//
-//          activeQuests.forEach((activeQuest) -> {
-//            questsService.getQuestById(activeQuest.getQuestId()).thenAccept((quest) -> {
-//
-//              // The quest doesn't exist
-//              if (quest.isEmpty()) {
-//                return;
-//              }
-//
-//              QuestObjectiveDto objective = quest.get().getObjective();
-//              if (objective.getType() == ObjectiveType.KILL_PLAYERS) {
-//                activeQuest.setProgress(activeQuest.getProgress() + 1);
-//              }
-//
-//            });
-//
-//          });
-//
-//        });
-//
-//  }
+  @EventHandler
+  public void onMobKill(EntityDeathEvent event) {
+
+    Player player = event.getEntity().getKiller();
+    Entity killedEntity = event.getEntity();
+
+    if (player == null) {
+      return;
+    }
+
+    activeQuestsService.getActiveQuestsForPlayer(player.getUniqueId())
+        .thenAccept((activeQuests) -> {
+
+          // The player doesn't have any active quests - no need to continue
+          if (activeQuests.isEmpty()) {
+            return;
+          }
+
+          activeQuests.forEach((activeQuest) -> {
+
+            // Even if the quest is expired, we still need to notify that the quest was updated.
+            // This is because we need to trigger the ActiveQuestUpdateEvent, so we can remove
+            // the quest if it is expired.
+            questsMediator.notifyQuestUpdate(player, activeQuest);
+
+            // If the quest is expired, we don't need to do anything with this event
+            if (activeQuestsService.isQuestExpired(activeQuest.getPlayerId(),
+                activeQuest.getQuestId())) {
+              return;
+            }
+
+            questsService.getQuestById(activeQuest.getQuestId(), true).thenAccept((quest) -> {
+
+              // The quest doesn't exist
+              if (quest.isEmpty()) {
+                return;
+              }
+
+              QuestObjectiveDto objective = quest.get().getObjective();
+              if (objective.getStringValue().equals(killedEntity.getType().toString())
+                  && objective.getType() == ObjectiveType.KILL_MOBS) {
+
+                questsMediator.updateQuestProgress(player, activeQuest);
+
+                // Send a message to the player, using the action bar, with the quest progress
+                sendProgressMessage(player, activeQuest);
+
+                if (activeQuestsService.isQuestCompleted(activeQuest.getPlayerId(),
+                    activeQuest.getQuestId())) {
+                  questsMediator.handleQuestCompletion(player, activeQuest);
+                }
+
+              }
+
+            });
+
+          });
+
+        });
+
+  }
 
   private void sendProgressMessage(Player player, ActiveQuestDto activeQuest) {
     player.sendActionBar(
